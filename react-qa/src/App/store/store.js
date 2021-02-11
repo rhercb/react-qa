8366bc8a99abd877aae7402c84a8baf0d8ff27b5
rhercb <rhercb@gmail.com>
2021-02-11 20:10:20 +0200

    Add basic redux saga implementation

---
+++ react-qa/src/App/store/store.js
@@ -0,0 +1,15 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import createSagaMiddleware from "redux-saga";
+
+import rootReducer from "./reducer";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(sagaMiddleware))
+);
+
+export default store;
